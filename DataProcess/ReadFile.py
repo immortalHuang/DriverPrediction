9877876204916094c12e6178796341780080ab24
immortal <huangmingle@zhiweicloud.com>
2017-11-07 20:22:18 +0800

    add spark

--- DataProcess/ReadFile.py
+++ DataProcess/ReadFile.py
@@ -1,9 +1,39 @@
+import os
+
 import pandas as pd
-import numpy as np
+from pyspark.sql import SparkSession
+from sklearn import svm
+#
+OriginTrainData = pd.read_csv("../Data/train.csv")
+OriginTestData = pd.read_csv("../Data/test.csv")
 
-TrainData = pd.read_csv("../Data/train.csv")
-TestData = pd.read_csv("../Data/test.csv")
+TrainDataY = OriginTrainData.pop('target')
+OriginTrainData.insert(0,'target',TrainDataY)
 
-Head5 = TrainData.head()
 
-print Head5
+#
+# clf = svm.SVC()
+#
+# clf.fit(TrainDataX,TrainDataY)
+#
+# result = clf.predict(OriginTestData)
+#
+# print result.head()
+
+from pyspark.ml.classification import LinearSVC
+
+spark = SparkSession \
+    .builder \
+    .appName("linearSVC") \
+    .getOrCreate() \
+    .master("spark://127.0.1.1:7077")
+
+
+lsvc = LinearSVC(maxIter=10, regParam=0.1)
+
+lsvcModel = lsvc.fit(OriginTrainData)
+
+print("Coefficients: " + str(lsvcModel.coefficients))
+print("Intercept: " + str(lsvcModel.intercept))
+
+spark.stop()
