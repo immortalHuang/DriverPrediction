9f658f067db6b80798a3a44f43df961232c102f0
immortal <huangmingle@zhiweicloud.com>
2017-11-10 17:00:44 +0800

    spark first run

--- DataProcess/ReadFile.py
+++ DataProcess/ReadFile.py
@@ -1,60 +1,40 @@
 # -*- coding: utf-8 -*-
-import os
-
-import pandas as pd
 from pyspark.ml.linalg import VectorUDT
 from pyspark.mllib.regression import LabeledPoint
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructField, StructType, DoubleType
-from sklearn import svm
-#
-
-
-
-#
-# clf = svm.SVC()
-#
-# clf.fit(TrainDataX,TrainDataY)
-#
-# result = clf.predict(OriginTestData)
-#
-# print result.head()
-
 from pyspark.ml.classification import LinearSVC
 from pyspark.sql import SQLContext
+from pyspark.ml.linalg import Vectors
+import sys
 
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def process(s):
+    words = s.split(',')
+    return (words[1],Vectors.dense([float(words[2]),float(words[3])]))
 
 spark = SparkSession \
     .builder \
     .appName("linearSVC") \
-    .master("local[4]") \
+    .master("spark://127.0.1.1:7077") \
     .getOrCreate()
 
 sqlContext = SQLContext(spark)
 
 sc = spark.sparkContext
 
-
-DataStruct = StructType([StructField("label", DoubleType(), True),StructField("features", VectorUDT(), True)])
-TrainData = spark.read.csv(
-    "../Data/train.csv", header=True, mode="DROPMALFORMED"
-)
+OriginTrainData = sc.textFile("file:///mnt/f/workspace/DriverPrediction/Data/train.csv").map(lambda x:x.split(',')).filter(lambda x:x[0]!='id').map(lambda x:tuple((float(x[1]),Vectors.dense([float(x[2]),float(x[3])])))).collect()
+SparkTrainData = spark.createDataFrame(OriginTrainData,['label','features'])
 
 
-TrainData.withColumn('id2',TrainData.id+1)
 
+lsvc = LinearSVC(maxIter=10, regParam=0.1)
 
-OriginTestData = sc.textFile("../Data/test.csv")
-TestData = spark.read.csv(
-    "../Data/test.csv", header=True, mode="DROPMALFORMED"
-)
+lsvcModel = lsvc.fit(SparkTrainData)
 
+print("Coefficients: " + str(lsvcModel.coefficients))
+print("Intercept: " + str(lsvcModel.intercept))
 
-# lsvc = LinearSVC(maxIter=10, regParam=0.1)
-#
-# lsvcModel = lsvc.fit(TrainData)
-#
-# print("Coefficients: " + str(lsvcModel.coefficients))
-# print("Intercept: " + str(lsvcModel.intercept))
-
-spark.stop()
+# spark.stop()
