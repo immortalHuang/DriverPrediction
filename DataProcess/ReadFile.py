0c89526a60709ce0cac93f33dcd6218bee7ec4c2
immortal <huangmingle@zhiweicloud.com>
2017-11-16 17:16:27 +0800

    spark predict

--- DataProcess/ReadFile.py
+++ DataProcess/ReadFile.py
@@ -1,29 +1,36 @@
 # -*- coding: utf-8 -*-
-
+from pyspark.mllib.tree import DecisionTree
 from pyspark.sql import SparkSession
 from pyspark.ml.classification import LinearSVC
-from pyspark.sql import SQLContext
 from pyspark.ml.linalg import Vectors
 
 spark = SparkSession \
     .builder \
     .appName("linearSVC") \
-    .master("local[*]") \
+    .master("spark://127.0.1.1:7077") \
+    .config("spark.driver.memory","1g") \
     .getOrCreate()
-
-sqlContext = SQLContext(spark)
 
 sc = spark.sparkContext
 
 OriginTrainData = sc.textFile("../Data/train.csv").map(lambda x:x.split(',')).filter(lambda x:x[0]!='id').map(lambda x:tuple((float(x[1]),Vectors.dense([float(x[i]) for i in range(0,len(x)-1) if i!=1])))).collect()
 SparkTrainData = spark.createDataFrame(OriginTrainData,['label','features'])
 
+(trainingData, testData) = SparkTrainData.randomSplit([0.7, 0.3])
 
-lsvc = LinearSVC(maxIter=10, regParam=0.1)
+model = DecisionTree.trainRegressor(trainingData, categoricalFeaturesInfo={},
+                                    impurity='variance', maxDepth=5, maxBins=32)
 
-lsvcModel = lsvc.fit(SparkTrainData)
+predictions = model.predict(testData.map(lambda x: x.features))
 
-print("Coefficients: " + str(lsvcModel.coefficients))
-print("Intercept: " + str(lsvcModel.intercept))
+predictions.show()
+# lsvc = LinearSVC(maxIter=10, regParam=0.1)
+#
+# lsvcModel = lsvc.fit(SparkTrainData)
+#
+# result = lsvcModel.pre
+#
+# print("Coefficients: " + str(lsvcModel.coefficients))
+# print("Intercept: " + str(lsvcModel.intercept))
 
-# spark.stop()
+spark.stop()
