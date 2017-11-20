1e10e7cfce20c1626ae78287e6f390e17fb8f580
immortalHuang <1183768063@qq.com>
2017-11-20 15:47:13 +0800

    add examples

--- DataProcess/ReadFile.py
+++ DataProcess/ReadFile.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
+from pyspark.mllib.regression import LabeledPoint
 from pyspark.mllib.tree import DecisionTree
 from pyspark.sql import SparkSession
-from pyspark.ml.classification import LinearSVC
-from pyspark.ml.linalg import Vectors
 
 spark = SparkSession \
     .builder \
@@ -13,17 +12,18 @@ spark = SparkSession \
 
 sc = spark.sparkContext
 
-OriginTrainData = sc.textFile("../Data/train.csv").map(lambda x:x.split(',')).filter(lambda x:x[0]!='id').map(lambda x:tuple((float(x[1]),Vectors.dense([float(x[i]) for i in range(0,len(x)-1) if i!=1])))).collect()
-SparkTrainData = spark.createDataFrame(OriginTrainData,['label','features'])
 
-(trainingData, testData) = SparkTrainData.randomSplit([0.7, 0.3])
+OriginTrainData = sc.textFile("../Data/train.csv").map(lambda x:x.split(',')).filter(lambda x:x[0]!='id').map(lambda x:LabeledPoint(x[1],[float(x[i]) for i in range(0,len(x)-1) if i!=1 and i!=0]))
+# SparkTrainData = spark.createDataFrame(OriginTrainData,['label','features'])
 
-model = DecisionTree.trainRegressor(trainingData, categoricalFeaturesInfo={},
+(trainingData, testData) = OriginTrainData.randomSplit([0.7, 0.3])
+
+model = DecisionTree.trainRegressor(trainingData, categoricalFeaturesInfo={0:2},
                                     impurity='variance', maxDepth=5, maxBins=32)
 
-predictions = model.predict(testData.map(lambda x: x.features))
+predictions = model.predict(testData.map(lambda x:x.features))
 
-predictions.show()
+print predictions.collect()
 # lsvc = LinearSVC(maxIter=10, regParam=0.1)
 #
 # lsvcModel = lsvc.fit(SparkTrainData)
---
+++ DataProcess/examples.py
@@ -0,0 +1,52 @@
+from string import Template
+
+from pyspark.ml import Pipeline
+from pyspark.ml.classification import DecisionTreeClassifier
+from pyspark.ml.feature import VectorAssembler, StringIndexer, VectorIndexer, IndexToString
+from pyspark.mllib.tree import DecisionTree
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("Spark decision tree classifier").getOrCreate()
+
+dataList = [
+    (0, "female", 32, 1.5, "no", 2, 17, 5, 5),
+    (0, "male", 57, 15.0, "yes", 5, 18, 6, 5),
+    (0, "female", 32, 15.0, "yes", 1, 12, 1, 4),
+    (0, "female", 27, 4.0, "no", 4, 14, 6, 4),
+    (0, "male", 37, 10.0, "no", 3, 18, 7, 4),
+    (0, "male", 22, 0.75, "no", 2, 17, 6, 3)
+]
+
+data = spark.createDataFrame(dataList,["affairs", "gender", "age", "yearsmarried", "children", "religiousness", "education", "occupation", "rating"])
+
+data.createOrReplaceTempView("data")
+
+labelWhere = "case when affairs=0 then 0 else cast(1 as double) end as label"
+genderWhere = "case when gender='female' then 0 else cast(1 as double) end as gender"
+childrenWhere = "case when children='no' then 0 else cast(1 as double) end as children"
+
+dataLabelDF = spark.sql(Template('select ${labelWhere}, ${genderWhere},age,yearsmarried,${childrenWhere},religiousness,education,occupation,rating from data'). \
+substitute(labelWhere=labelWhere,genderWhere=genderWhere,childrenWhere=childrenWhere))
+
+featuresArray = ["gender", "age", "yearsmarried", "children", "religiousness", "education", "occupation", "rating"]
+
+assembler = VectorAssembler().setInputCols(featuresArray).setOutputCol("features")
+vecDF = assembler.transform(dataLabelDF)
+
+labelIndexer = StringIndexer(inputCol="label", outputCol="indexedLabel").fit(vecDF)
+
+featureIndexer = VectorIndexer(inputCol="features", outputCol="indexedFeatures").fit(vecDF)
+(trainingData, testData) = vecDF.randomSplit([0.7, 0.3])
+
+dt = DecisionTreeClassifier(labelCol="indexedLabel", featuresCol="indexedFeatures",maxBins=32,
+                            impurity="entropy",minInfoGain=0.01,minInstancesPerNode=10,seed=123456)
+
+labelConverter = IndexToString(inputCol="prediction", outputCol="predictedLabel").setLabels(labelIndexer.labels)
+
+pipeline = Pipeline(stages=[labelIndexer, featureIndexer, dt, labelConverter])
+
+model = pipeline.fit(trainingData)
+
+predictions = model.transform(testData)
+
+predictions.select("predictedLabel", "label", "features").show(10, truncate = False)
